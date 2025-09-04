e448add33367d7422b77f49b7d6790e93dc17fbf
Marryam Mouslimany <92210054@students.liu.edu.lb>
2025-09-05 01:23:12 +0300

    added the filtering logic to the get users api
    fixes STU-5

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,18 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreUserRequest;
 use App\services\UserService;
+use App\http\Requests\GetUserRequest;
 
 class UserController extends Controller
 {
-    public function getUsers()
+    public function getUsers(GetUserRequest $request)
     {
         try {
-            $users = UserService::getUsers();
+            $users = UserService::getUsers($request->validated()['filters'] ?? []);
             if (!$users)
                 return $this->error('Users Not Found');
             return $this->success($users);
         } catch (\Exception $e) {
-            return $this->error('Something went wrong', 500);
+            return $this->error($e->getMessage(), 500);
         }
     }
 
